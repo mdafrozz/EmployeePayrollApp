0178258ea5c3a3a59d762a15f74f51211b89218e
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-10-29 07:41:51 +0530

    Refactor: Added Lombok library, Exception handlers, ResponseDTO and Validations

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -2,13 +2,21 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		System.out.println("Welcome to Employee Payroll App..!!");
+		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll Application Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
+        log.info("Employee Payroll DB User is {} ",
+                context.getEnvironment().getProperty("spring.datasource.username"));
+        System.out.println("Welcome to Employee Payroll App..!!");   		
 	}
-
+	  
 }
